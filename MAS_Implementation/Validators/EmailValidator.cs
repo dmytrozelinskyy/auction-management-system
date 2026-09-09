c21e6a61bcaf765ba101befb283ce787f167583a
Dmytro Zelinskyy <dmytrozelinskyy07@gmail.com>
2026-09-09 21:31:09 +0200

    feat: add email validation service

---
+++ MAS_Implementation/Validators/EmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MAS_Implementation;
+
+public static class EmailValidator
+{
+    private static readonly Regex Pattern =
+        new(@"[a-zA-Z0-9._%+-]+@[a-zA-z0-9.-]+\.[a-zA-z]{2,}$",
+            RegexOptions.Compiled | RegexOptions.CultureInvariant);
+    
+    public static bool IsValidEmail(string email) =>
+        !string.IsNullOrWhiteSpace(email) && Pattern.IsMatch(email);
+}
